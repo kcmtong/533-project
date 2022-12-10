ccf8c9cfa2408807d97b3be3cdd7f9299b751495
kcmtong <cmtong1108@gmail.com>
2022-12-10 00:35:01 -0800

    complete the Analyzer pkg and Resource classes

--- DLAdvisor/Analyzer/Advisor.py
+++ DLAdvisor/Analyzer/Advisor.py
@@ -1,8 +1,94 @@
-from ProfileBuilder import Collector
+from DLAdvisor.ProfileBuilder import Collector
+from DLAdvisor.Analyzer import Resource
 
+# TEST ONLY
 def kttest1():
     Collector.greeting()
+
+def advice(stage) :
+    prepRes = populatePrepResources(stage)
+    OLRes = populateOLResources(stage)
+    if (stage == 1) :
+        objective = "pass the ICBC Knowledge Test"
+    elif (stage == 2) :
+        objective = "pass the ICBC Road Test 5"
+    elif (stage == 3) :
+        objective = "pass the ICBC Road Test 7"
+    else :
+        objectove = "apply exchanging for your license from Reciprocal countries"
+        
+    print(f'Here are your suggested resources to {objective}\n##########' )
+    prepRes.display()
+    print("")
+    OLRes.display()
+    print("########")
     
-def advisor_print():
-    print("print from advisor")
+## TODO : Provide Resources
+def populatePrepResources(stage):
     
+    res1 = [
+        ['Learn stage 1/Knowledge Test','http://www.stage1.com'],
+        ['Kelowna Best Driving School','https://threebestrated.ca/driving-schools-in-kelowna-bc'],
+        ['BC Driving Tips','https://www.youtube.com/watch?v=UohMY0CT_V8']
+        ]
+    
+    res2 = [
+        ['Learn stage 2/RoadTest7','http://www.stage2.com'],
+        ['Kelowna Best Driving School','https://threebestrated.ca/driving-schools-in-kelowna-bc'],
+        ['BC Driving Tips','https://www.youtube.com/watch?v=UohMY0CT_V8']
+        ]
+    
+    res3 = [
+        ['Learn stage 3/RoadTest5','http://www.stage3.com'],
+        ['Kelowna Best Driving School','https://threebestrated.ca/driving-schools-in-kelowna-bc'],
+        ['BC Driving Tips','https://www.youtube.com/watch?v=UohMY0CT_V8']
+        ]
+    
+    res4 = [
+        ['Learn stage 4/RecipLic','http://www.stage4.com'],
+        ['Kelowna Best Driving School','https://threebestrated.ca/driving-schools-in-kelowna-bc'],
+        ['BC Driving Tips','https://www.youtube.com/watch?v=UohMY0CT_V8']
+        ]
+
+    if (stage == 1) :
+        res = res1
+    elif (stage == 2) :
+        res = res2
+    elif (stage == 3) :
+        res = res3
+    else :
+        res = res4
+        
+    return Resource.PrepResource(stage,res)
+
+
+# TODO : Provide Resources
+def populateOLResources(stage):
+
+    res1 = [
+        ['Learn stage 1/Knowledge Test OL','http://www.stage1.com']
+        ]
+    
+    res2 = [
+        ['Learn stage 2/RoadTest7 OL','http://www.stage2.com'],
+        ['Kelowna Best Driving School','https://threebestrated.ca/driving-schools-in-kelowna-bc']
+        ]
+    
+    res3 = [
+        ['Learn stage 3/RoadTest5 OL','http://www.stage3.com']
+    ]
+    
+    res4 = [
+        ['Learn stage 4/RecipLic OL','http://www.stage4.com']
+        ]
+
+    if (stage == 1) :
+        res = res1
+    elif (stage == 2) :
+        res = res2
+    elif (stage == 3) :
+        res = res3
+    else :
+        res = res4
+        
+    return Resource.OLResource(stage,res)
--- DLAdvisor/Main.py
+++ DLAdvisor/Main.py
@@ -1,4 +1,7 @@
 from DLAdvisor.ProfileBuilder import Collector, UserProfile
+from DLAdvisor.Analyzer import Advisor
+from time import sleep
+from tqdm import tqdm
 
 def execute():
     Collector.greeting()
@@ -6,11 +9,22 @@ def execute():
 #        print("Sorry, not eligible! Bye!")
 #        return
     if (not Collector.passBasicEligibility()) :
-        print("Sorry, too young!")
+        print("Sorry, you are not eligible!")
         return
-    print("Great! You can proceed2")
+    print("Great! you are eligible to proceed!")
     user = Collector.gatherProfile()    
     user.formatName()
-    print("Returned User Profile ....." + str(user))
+    print("Here is your profile summary\n######" + str(user) +
+          "\n######")
+
+    print("Analyzing...")
+    for i in tqdm(range(5)):
+        sleep(0.5)
     
     # TODO BY KENNY
+    # temp set all user.stage = 1, to skip the following line when UserProfile pkg is completed
+    user.stage = 1
+    Advisor.advice(user.stage)
+    
+    
+    
